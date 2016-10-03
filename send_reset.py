8308ed198cd3db5773c7da552543ee86505cfd76
Raymond Williams <GunaksGit@gmail.com>
2016-10-03 12:26:01 -0700

    got selenium to run and reset password X times per user

--- send_reset.py
+++ send_reset.py
@@ -15,6 +15,54 @@ class Reset(object):
         self.attemps = attemps
         self.headless = headless
 
-        # if headless run selenium
+        if self.headless == True:
+            self.use_requests()
+        else:
+            self.use_selenium()
 
-        # if not headless run requests
+
+    def use_selenium(self):
+        count = 0
+
+        driver = webdriver.Firefox()
+        driver.get(self.url)
+
+        while count < self.attemps:
+
+            #driver = webdriver.Firefox()
+            #driver.get(self.url)
+
+            wait = ui.WebDriverWait(driver, 10)
+            wait.until(page_is_loaded)
+
+	    # input apple id into search field
+            elem = driver.find_element_by_xpath("//input[@id='appleid']")
+            elem.send_keys(self.apple_id)
+            elem.send_keys(Keys.RETURN)
+
+            wait = ui.WebDriverWait(driver, 10)
+            wait.until(page_is_loaded)
+            
+            elem2 = driver.find_element_by_xpath("//div[@class='text-centered']/div[@class='btn-group flow-controls']/button[@id='action']")
+            elem2.click()
+
+            wait = ui.WebDriverWait(driver, 10)
+            wait.until(page_is_loaded)
+
+            elem3 = driver.find_element_by_xpath("//button[@class='btn blue-btn btn-link iforgot-btn done link']")
+            elem3.click()
+
+            count += 1
+            print ("Attempt %d done" % count)
+            sleep(2)
+
+            sleep(2)
+            driver.quit()
+
+    def use_requests(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    test = Reset("test@test.test", attemps=2)
